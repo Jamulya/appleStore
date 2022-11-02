f332b6df5b24878fd81a81e0f1ec98e1146353ae
jamulya <jamka_kg@mail.ru>
2022-11-02 23:25:51 +0600

    lesson-8

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 let url = 'http://localhost:3000/products?'
 
+let basket = [
+
+]
+
+
 let overlay = document.querySelector('.overlay')
 let addBtn = document.querySelector('.add-btn')
 let formClose = document.querySelector('.form__close')
@@ -8,17 +13,63 @@ let form = document.querySelector('.form')
 let seeAll = document.querySelector('.products__see')
 let formChange = document.querySelector('.form-change')
 let productsItem = document.querySelectorAll('.products__item')
+let basketCount = document.querySelector('.header__list-cart')
+let openBasket = document.querySelector('.header__item_cart')
+let basketModal = document.querySelector('.basket')
+let basketRow = document.querySelector('.basket__row')
+let totalBasket = document.querySelector('.basket__end-value.total')
+let saleBasket = document.querySelector('.basket__end-value.sale')
+let headerTotal = document.querySelector('.header__list-price')
+
+
+openBasket.addEventListener('click', () => {
+    overlay.style.display = 'block'
+    basketModal.style.display = 'block'
+    basketRow.innerHTML = ''
+
+
+    basket.forEach((item) => {
+        basketRow.innerHTML += `
+                            <div class="basket__card">
+                            <div class="basket__card-left">
+                            <img class="basket__card-img" src="${item.image}" alt="">
+                           <div class="basket__card-center">
+                            <h2 class="basket__card-title">${item.title}</h2>
+                            <p class="basket__card-price">${item.price} $</p>
+                            <div>
+                                <button class="basket__card-plus">+</button><span>${item.count}</span><button class="basket__card-minus">-</button>
+                            </div>
+                        </div>
+                     </div>
+                        <button class="basket__card-btn">X</button>
+             </div>
+        `
+    })
+    totalBasket.textContent = `${basket.reduce((acc, rec) => {
+        return acc + rec.price * rec.count
+    }, 0)} $`
+
+
+    saleBasket.textContent = `${basket.reduce((acc, rec) => {
+        return acc + rec.price * rec.count
+    }, 0) / 100 * 5} $`
+})
+
 
 
 
 addBtn.addEventListener('click', function () {
     overlay.style.display = 'block'
     form.style.display = 'flex'
+    formChange.style.display = 'none'
 })
 
 formClose.addEventListener('click', function () {
     overlay.style.display = 'none'
     form.style.display = 'none'
+    formChange.style.display = 'none'
+    basketModal.style.display = 'none'
+
 
 })
 
@@ -26,6 +77,8 @@ overlay.addEventListener('click', function (e) {
     if (e.target.className.includes('overlay')) {
         overlay.style.display = 'none'
         form.style.display = 'none'
+        formChange.style.display = 'none'
+        basketModal.style.display = 'none'
 
     }
 })
@@ -34,10 +87,19 @@ let all = ''
 let status = 'All'
 
 
+const refreshForm = (e) => {
+    e.target[0].value = ''
+    e.target[1].value = ''
+    e.target[2].value = ''
+    e.target[3].value = ''
+    e.target[4].value = ''
+    overlay.style.display = 'none'
+    getProducts()
+}
 
 
 //products__row
-const getProducts = () => {
+function getProducts  () {
     productsRow.innerHTML = ''
     fetch(url + `${all.length ? '' : '_limit=4&'}${status === 'All' ? '' : 'category=' + status}`)
     .then((res) => res.json())
@@ -53,7 +115,7 @@ const getProducts = () => {
                 $${item.price}
             </p>
             <div class="products__card-btns">
-                <button class="products__card-btn">
+                <button  data-id = "${item.id}" class="products__card-btn products__card-basket">
                     Buy
                 </button>
                 <button data-id = "${item.id}"  class="products__card-btn products__card-change">
@@ -111,30 +173,42 @@ const getProducts = () => {
                         },
                         body: JSON.stringify(product)
                     }).then((res) => {
-                        // e.target[0].value = ''
-                        // e.target[1].value = ''
-                        // e.target[2].value = ''
-                        // e.target[3].value = ''
-                        // e.target[4].value = ''
-                        // overlay.style.display = 'none'
-                        // getProducts(e)
-                    })
+                        refreshForm()
+                    }).catch(() => alert('Ошибка при добавлении'))
                 })
-                // fetch(`http://localhost:3000/products/${change.dataset.id}`, {
-                //     method: 'PATCH',
-                //     headers: {
-                //         'Content-Type': 'application/json'
-                //     },
-                //     body: JSON.stringify(product)
-                // }).then((res) => {
-                //     e.target[0].value = ''
-                //     e.target[1].value = ''
-                //     e.target[2].value = ''
-                //     e.target[3].value = ''
-                //     e.target[4].value = ''
-                //     overlay.style.display = 'none'
-                //     getProducts(e)
-                // }).catch(() => alert('Ошибка при добавлении'))
+               
+            })
+
+        })
+
+        let basketBtn = document.querySelectorAll('.products__card-basket')
+        Array.from(basketBtn).forEach((btn) => {
+            btn.addEventListener('click', () => {
+                fetch(`http://localhost:3000/products/${btn.dataset.id}`)
+                .then((res) => res.json())
+                .then((res) => {
+                    // basket = [...basket, res]
+                    // console.log(basket);
+
+                    let have = basket.findIndex(el => el.id === res.id)
+                    // console.log(have);
+
+                    if(have >= 0){
+                        basket[have] = {...basket[have], count: basket[have].count+1}
+                    }else{
+                        basket = [...basket, {
+                        ...res,
+                        count: 1
+                    }]
+                    }
+                    
+                    basketCount.textContent = basket.length
+
+                    headerTotal.textContent = `${basket.reduce((acc, rec) => {
+                        return acc + rec.price * rec.count
+                    }, 0)} $`
+                    
+                }).catch(() => alert('Ошибка при изменения товара'))
             })
         })
 
@@ -195,10 +269,6 @@ seeAll.addEventListener('click', () => {
 
 Array.from(productsItem).forEach((item) => {
     item.addEventListener('click', () => {
-        // item.classList.add('products__item_active')
-        // status =item.textContent
-        // console.log(status);
-
         Array.from(productsItem).forEach((el) => {
             if(el.textContent === item.textContent){
                 el.classList.add('products__item_active')
